7a4f5c7488e87fc0a1330d56add94d751fa52dda
arpit0510 <arpitgupta11103448@gmail.com>
2025-09-10 12:30:17 +0100

    adding husky and prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import type { Request, Response } from "express";
+import express from 'express';
+import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,14 +7,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req : Request, resp : Response) => {
-    resp.send(process.env.OPENAI_API_KEY);
+app.get('/', (req: Request, resp: Response) => {
+  resp.send(process.env.OPENAI_API_KEY);
 });
 
-app.get('/api/hello', (req : Request, resp : Response) => {
-    resp.json({'message' : 'Hello Daddy !'});
+app.get('/api/hello', (req: Request, resp: Response) => {
+  resp.json({ message: 'Hello Daddy !' });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
