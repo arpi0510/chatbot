cbcb4a7e62b841419251635fb29f8409103ad376
arpit0510 <arpitgupta11103448@gmail.com>
2025-09-11 09:27:32 +0100

    Converting to chat bot form

--- packages/client/src/components/Chatbot.tsx
+++ packages/client/src/components/Chatbot.tsx
@@ -1,18 +1,45 @@
+import { useForm } from 'react-hook-form';
 import { Button } from './ui/button';
 import { FaArrowUp } from 'react-icons/fa';
+import type { KeyboardEvent } from 'react';
+
+type FormData = {
+  prompt: string;
+};
 
 const Chatbot = () => {
+  const { register, handleSubmit, reset, formState } = useForm<FormData>();
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+    reset();
+  };
+
+  const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
+    if (e.key == 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit(onSubmit)();
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-2 items-end border-2 p-3 rounded-3xl">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      onKeyDown={onKeyDown}
+      className="flex flex-col gap-2 items-end border-2 p-3 rounded-3xl"
+    >
       <textarea
+        {...register('prompt', {
+          required: true,
+          validate: (message) => message.trim().length > 0,
+        })}
         className="w-full border-0 focus:outline-0 resize-none"
         placeholder="Ask anything"
         maxLength={1000}
       ></textarea>
-      <Button className="w-9 h-9 rounded-3xl">
+      <Button disabled={!formState.isValid} className="w-9 h-9 rounded-3xl">
         <FaArrowUp />
       </Button>
-    </div>
+    </form>
   );
 };
 
